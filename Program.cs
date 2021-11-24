41d18597db81c85cddd817aafc891dd3abe61b22
Rodrigo Haneiko <rodsbr@gmail.com>
2021-11-23 22:20:21 -0300

    Adiciondo comentário

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Course {
 
             Console.Write("Digite o numero de linhas da Matriz: ");
             int n = int.Parse(Console.ReadLine());
-
+             //qualquer coisa aqyu
             mat = new int[n, n];
             int neg = 0;
 
